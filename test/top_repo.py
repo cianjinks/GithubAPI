4af670197a5c8f4b06d5a929fa1e152b417e2995
Cian Jinks <cjinks99@gmail.com>
2021-12-20 20:02:06 +0000

    Basic flask website and data visualization

--- test/top_repo.py
+++ test/top_repo.py
@@ -19,22 +19,28 @@ repos = {
 token = os.getenv('GITHUB_TOKEN', '...')
 headers = {
     'Authorization': f'token {token}',
-    'Accept': 'application/vnd.github.v3+json'  
+    'Accept': 'application/vnd.github.v3+json'
 }
 
 def get_repo(owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}"
+    url = f"https://api.github.com/repos/{owner}/{repo}/contributors"
     r = requests.get(url, headers=headers)
     repo_data = r.json()
-    # print(json.dumps(repo_data, indent=4))
-    stars = repo_data["stargazers_count"]
-    forks = repo_data["forks_count"]
-    open_issues = repo_data["open_issues"]
 
-    print(f"Stats for {owner}/{repo}")
-    print(f"    Stars - {stars}")
-    print(f"    Forks - {forks}")
-    print(f"    Open Issues - {open_issues}")
+    print(f"Contributors of {owner}/{repo}")
+    for contributor in repo_data:
+        user = contributor["login"]
+        total_contributions = contributor["contributions"]
+        print(f"{user} - {total_contributions}")
+    # print(json.dumps(repo_data, indent=4))
+    # stars = repo_data["stargazers_count"]
+    # forks = repo_data["forks_count"]
+    # open_issues = repo_data["open_issues"]
+
+    # print(f"Stats for {owner}/{repo}")
+    # print(f"    Stars - {stars}")
+    # print(f"    Forks - {forks}")
+    # print(f"    Open Issues - {open_issues}")
 
     return repo_data
 
